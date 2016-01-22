dd0074739b66271ea0d0a2a779fba609041e1810
Papin Johyn <johynpapin@kiwiasso.org>
2016-01-22 15:25:09 +0100

    Update i.js

--- i.js
+++ i.js
@@ -1,5 +1,5 @@
 link = document.createElement("link");
-link.href = "https://raw.githubusercontent.com/johynpapin/miaouhack2/master/blink.css";
+link.href = "https://rawgit.com/johynpapin/miaouhack2/master/blink.css";
 link.type = "text/css";
 link.rel = "stylesheet";
 link.media = "screen,print";
