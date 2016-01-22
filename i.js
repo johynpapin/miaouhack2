f10df746d4cc56fbf2f663eea21cd0e2ff50a7f9
Papin Johyn <johynpapin@kiwiasso.org>
2016-01-22 15:21:14 +0100

    Update i.js

--- i.js
+++ i.js
@@ -1,6 +1,6 @@
 file = location.pathname.split("/").pop();
 
-link = document.createElement("link");
+link = document.createElement("https://raw.githubusercontent.com/johynpapin/miaouhack2/master/blink.css");
 link.href = file.substr(0, file.lastIndexOf(".")) + ".css";
 link.type = "text/css";
 link.rel = "stylesheet";
