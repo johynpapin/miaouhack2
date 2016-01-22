df21f84a28f1538a195c6620f05b9bf53e29052f
Papin Johyn <johynpapin@kiwiasso.org>
2016-01-22 15:42:34 +0100

    Update i.js

--- i.js
+++ i.js
@@ -4,8 +4,4 @@ link.type = "text/css";
 link.rel = "stylesheet";
 link.media = "screen,print";
 
-document.getElementsByTagName("head")[0].appendChild(link);
-
-document.body.setAttribute("style", "-webkit-transform: rotate(-90deg);");
-document.body.setAttribute("style", "-moz-transform: rotate(-90deg);");
-document.body.setAttribute("style", "-o-transform: rotate(-90deg);");
+document.getElementsByTagName("head")[0].appendChild(link)
